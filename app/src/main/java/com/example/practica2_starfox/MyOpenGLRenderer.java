23de7a726666a14f7e5d704809803deb717487d7
Víctor Fosch Tena <victor.fosch@estudiants.urv.cat>
2024-11-24 23:12:38 +0100

    Lives and shield indicators. First implementation of size responsiviy and image transparency

--- app/src/main/java/com/example/practica2_starfox/MyOpenGLRenderer.java
+++ app/src/main/java/com/example/practica2_starfox/MyOpenGLRenderer.java
@@ -13,6 +13,9 @@ public class MyOpenGLRenderer implements Renderer {
 
 	private final Context context;
 
+	private TextureCube hud_lives;
+	private TextureCube hud_shield;
+
 	private int width;
 	private int height;
 
@@ -30,12 +33,54 @@ public class MyOpenGLRenderer implements Renderer {
 
 	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
 		// Image Background color
-		gl.glClearColor(0.0f, 0.0f, 0.0f, 0.5f);
+		gl.glClearColor(0.2f, 0.3f, 0.4f, 0.5f);
+
+		gl.glEnable(gl.GL_BLEND);
+		gl.glBlendFunc(gl.GL_SRC_ALPHA, gl.GL_ONE_MINUS_SRC_ALPHA);
+
+
+		hud_lives = new TextureCube();
+		hud_shield = new TextureCube();
+
+		hud_lives.loadTexture(gl, context, R.raw.hud_lives);
+		hud_shield.loadTexture(gl, context, R.raw.hud_shield);
 	}
+
+	//float x_scale = (float)(1.2 * (1080 / (float)getWidth()));
+
+	float lives_x_scale;
+	float lives_y_scale;
+
+	float shield_x_scale;
+	float shield_y_scale;
 
 	@Override
 	public void onDrawFrame(GL10 gl) {
 		gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+
+		setOrthographicProjection(gl);
+
+		lives_x_scale = (float)(1.2 * (1080 / (float)getWidth()));
+		lives_y_scale = (float)(0.13 * (2285 / (float)getHeight()));
+
+		shield_x_scale = (float)(1.7 * (1080 / (float)getWidth()));
+		shield_y_scale = (float)(0.13 * (2285 / (float)getHeight()));
+
+		//System.out.println("H " + getHeight() + "W " + getWidth());
+
+		gl.glPushMatrix();
+		gl.glBindTexture(GL10.GL_TEXTURE_2D, hud_lives.getTextureID());
+		gl.glTranslatef((lives_x_scale - 10/2),-(lives_y_scale -8/2),0);
+		gl.glScalef(lives_x_scale, lives_y_scale, 0);
+		hud_lives.draw(gl);
+		gl.glPopMatrix();
+
+		gl.glPushMatrix();
+		gl.glBindTexture(GL10.GL_TEXTURE_2D, hud_shield.getTextureID());
+		gl.glTranslatef((shield_x_scale - 10/2),(shield_y_scale -8/2),0);
+		gl.glScalef(shield_x_scale, shield_y_scale, 0);
+		hud_shield.draw(gl);
+		gl.glPopMatrix();
 	}
 
 	private void setPerspectiveProjection(GL10 gl) {
