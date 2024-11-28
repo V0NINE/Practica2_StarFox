6ced888d69664d621de21c6797919a7bddaa7ac6
Víctor Fosch Tena <victor.fosch@estudiants.urv.cat>
2024-11-28 17:21:08 +0100

    Starship movement arround the screen with border limits + light

--- app/src/main/java/com/example/practica2_starfox/MainActivity.java
+++ app/src/main/java/com/example/practica2_starfox/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.practica2_starfox;
 import android.os.Bundle;
 import android.app.Activity;
 import android.opengl.GLSurfaceView;
+import android.view.MotionEvent;
 
 public class MainActivity extends Activity {
 
     MyOpenGLRenderer myGLRenderer;
+    float previous_x, previous_y;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -15,5 +17,28 @@ public class MainActivity extends Activity {
         GLSurfaceView view = new GLSurfaceView(this);
         view.setRenderer(myGLRenderer = new MyOpenGLRenderer(this));
         setContentView(view);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent e) {
+        float x = e.getX();
+        float y = e.getY();
+
+        switch (e.getAction()) {
+            case MotionEvent.ACTION_MOVE:
+                float dx = x - previous_x;
+                float dy = y - previous_y;
+
+                System.out.println("Pipero " + myGLRenderer.getStarY() + " - " + myGLRenderer.getStarX());
+
+                //myGLRenderer.setStarY(myGLRenderer.getStarY() + ((dy>0)?-0.1f:0.1f));
+                myGLRenderer.setStarY(myGLRenderer.getStarY() + dy/130);
+                myGLRenderer.setStarX(myGLRenderer.getStarX() + dx/130);
+        }
+
+        previous_x = x;
+        previous_y = y;
+
+        return true;
     }
 }
--- app/src/main/java/com/example/practica2_starfox/MyOpenGLRenderer.java
+++ app/src/main/java/com/example/practica2_starfox/MyOpenGLRenderer.java
@@ -7,14 +7,18 @@ import android.content.Context;
 import android.opengl.GLSurfaceView.Renderer;
 import android.opengl.GLU;
 
-import org.w3c.dom.Text;
-
 public class MyOpenGLRenderer implements Renderer {
 
 	private final Context context;
 
+	private Object3D starwing;
+
+	private Light light;
+
 	private int width;
 	private int height;
+
+	private int frame;
 
 	public int getWidth() {
 		return width;
@@ -30,12 +34,50 @@ public class MyOpenGLRenderer implements Renderer {
 
 	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
 		// Image Background color
-		gl.glClearColor(0.0f, 0.0f, 0.0f, 0.5f);
+		gl.glClearColor(0.2f, 0.3f, 0.4f, 0.5f);
+
+		gl.glEnable(GL10.GL_LIGHTING);
+		gl.glEnable(GL10.GL_NORMALIZE);
+
+		light = new Light(gl, GL10.GL_LIGHT0);
+		light.setPosition(new float[] {2, 5, 5, 0});
+
+		light.setAmbientColor(new float[]{0.3f, 0.3f, 0.1f});
+		light.setDiffuseColor(new float[]{1f, 1f, 1f});
+
+		starwing = new Object3D(context, R.raw.starwing);
 	}
+
+	private float star_y;
+	private float star_x;
+
+	public void setStarY(float y) {
+		if(y > 6.3f)
+			this.star_y = 6.3f;
+		else this.star_y = Math.max(y, -6f);
+	}
+	public void setStarX(float x) {
+		if(x > 2.8f)
+			this.star_x = 2.8f;
+		else this.star_x = Math.max(x, -2.8f);
+	}
+
+	public float getStarY() { return this.star_y; }
+	public float getStarX() { return this.star_x; }
 
 	@Override
 	public void onDrawFrame(GL10 gl) {
 		gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+
+		setPerspectiveProjection(gl);
+
+		GLU.gluLookAt(gl, 0, 0.8f, 15, 0, 0, 0, 0, 1, 0);
+
+		gl.glPushMatrix();
+		gl.glTranslatef(star_x, -star_y,0);
+		gl.glRotatef(180,0, 1, 0 );
+		starwing.draw(gl);
+		gl.glPopMatrix();
 	}
 
 	private void setPerspectiveProjection(GL10 gl) {
@@ -68,6 +110,7 @@ public class MyOpenGLRenderer implements Renderer {
 		gl.glLoadIdentity();
 	}
 
+	private float aspect;
 	@Override
 	public void onSurfaceChanged(GL10 gl, int width, int height) {
 		// Define the Viewport
